8aea7b4b931f394a5a650eb1f0c9aae5d6c1c5bb
Frank Hofmann <frank.hofmann@efho.de>
2015-05-31 15:40:09 +0200

    example: hello world (output on stdout)

---
+++ hello-world/hello-world-2.tcl
@@ -0,0 +1,15 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# Hello, World!
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define string variable with content
+set message "Hello, World!"
+
+# output message on UNIX channel stdout
+puts stdout $message
