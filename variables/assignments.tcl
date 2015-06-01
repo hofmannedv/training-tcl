82ccffc7c4d602b25d490474d061b65b3b1f2102
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 01:05:20 +0200

    variables, assignments, and formatted output

---
+++ variables/assignments.tcl
@@ -0,0 +1,43 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# variables, assignments, and formatted output
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# single assignments
+# - string value
+set colour blue
+# - an integer value
+set size 17
+# - a float value
+set length 0.5
+
+puts "colour: ${colour}"
+puts "size: ${size}"
+puts "length: ${length}"
+
+# output as string
+puts [ format "%s" $colour ]
+
+# output as string, and value
+puts [ format "colour %s size %d" $colour $size ]
+
+# output as signed integer
+puts [ format "%d" $size ]
+
+# output as float value
+puts [ format "%f" $length ]
+
+# output as float value with 3 digits
+puts [ format "%.3f" $length ]
+
+# output as hexadecimal value
+puts [ format "%x" $size ]
+
+# output as hexadecimal value (extended)
+puts [ format "%#08X" $size ]
+
