0067ed21e9482efa6b19abfdd667a90d893c0e06
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 00:02:09 +0200

    variables, single values

---
+++ variables/variables.tcl
@@ -0,0 +1,15 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# Hello, World!
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# create message
+message .msg -text "Hallo, Welt!"
+
+# create output data for display window
+pack .msg
