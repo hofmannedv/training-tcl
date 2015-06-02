ac51a0daf109b2cf25230e8408cc8a1d97883ccf
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 23:51:30 +0200

    commandline: counting and evaluating arguments

---
+++ commandline/argv.tcl
@@ -0,0 +1,17 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# demonstrates the usage of commandline parameters
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# read number of arguments: argc
+puts "number of arguments: $argc"
+
+# read argument by argument: argv
+foreach item $argv {
+	puts "argument $item"
+}
