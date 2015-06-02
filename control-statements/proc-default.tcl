62814235a75730681deea9415fb506a6e2ab4c66
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 23:20:49 +0200

    control statements: procedures with default parameters

---
+++ control-statements/proc-default.tcl
@@ -0,0 +1,26 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# demonstrate the usage of procedures with default parameters
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+proc total {{operand1 0} {operand2 0}} {
+	# output the given parameters
+	puts "$operand1 $operand2"
+	return
+}
+
+# call procedure with two parameters
+# - with two parameters
+total 15 45
+
+# - with a single parameter, 2nd para has default value
+total 26
+
+# - without parameters, both of them are set to default values
+total
+
