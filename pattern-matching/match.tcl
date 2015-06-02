d44841cc3d48bbcf2be36087d097173627a9ffa1
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 14:17:28 +0200

    pattern matching: compare list with regex-like pattern

--- pattern-matching/match.tcl
+++ pattern-matching/match.tcl
@@ -11,18 +11,33 @@
 # define string
 set place Hamburg
 
-# define pattern with shell-like asterisk
+# define a pattern with shell-like asterisk
 set pattern1 H*
 
 if {[ string match $pattern1 $place ]} {
 	puts "pattern ${pattern1} found in ${place}"
 }
 
-# define pattern with a specific length
+# define a pattern with a specific length -- no matter of the content
 set pattern2 ??
 
 if {[ string match $pattern2 XY ]} {
 	puts "pattern ${pattern2} matches XY"
 }
 
+# define a regex-like pattern -- starting with either A, B or F
+set pattern3 {[ABF]*}
+
+# define list
+set cities "1 Augsburg 2 Berlin 3 Dresden 4 Frankfurt 5 Hamburg"
+puts $cities
+
+# transform this list into an array
+array set citiesArray $cities
+
+foreach element [array names citiesArray] { \
+	if {[ string match $pattern3 $citiesArray($element) ]} { \
+		puts "match: $citiesArray($element)" \
+	} \
+}
 
