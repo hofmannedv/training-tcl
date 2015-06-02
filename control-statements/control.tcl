6b1683577091ffc5cf5781acb16785647628eb23
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 22:13:03 +0200

    control structures: if-then-else and switch

---
+++ control-statements/control.tcl
@@ -0,0 +1,31 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# demonstrate control statements
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# if-then-else
+set value 45
+
+if {$value < 45} {
+	puts "value too small"
+} elseif {$value == 45} {
+	puts "correct value"
+} else {
+	puts "value too large"
+}
+
+# switch
+set pattern Hamburg
+
+switch -exact -- $pattern {
+	Hamburg { set value 50 }
+	Berlin { set value 60 }
+	Dresden { set value 80 }
+}
+
+puts "value $value"
