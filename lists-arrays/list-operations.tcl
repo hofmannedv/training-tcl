bd3a00017bfa76c8821a650b7b824c5b3c779ebb
Frank Hofmann <frank.hofmann@efho.de>
2015-06-02 02:05:06 +0200

    simple lists: adding a list item

---
+++ lists-arrays/list-operations.tcl
@@ -0,0 +1,18 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# list operations
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define a list of cities
+set cities "Lyon Metz Nancy Belfort"
+
+# add another place (at the end)
+lappend cities "Dijon"
+puts $cities
+
+
