6a59a7cbab77fb4547346e222011734596ed2b20
Frank Hofmann <frank.hofmann@efho.de>
2015-06-03 16:51:57 +0200

    client/server: basic client and server

---
+++ client-server/client.tcl
@@ -0,0 +1,33 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# demonstrates client-server communication (client side)
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define host
+set hostname localhost
+
+# define port
+set port 34567
+
+# define message
+set message "Hello, server!"
+
+# initiate connection to server via socket
+set channel [ socket $hostname $port ]
+
+# send data via socket
+puts $channel $message
+flush $channel
+puts "(client) sent to server: $message"
+
+# receive answer message, and output answer
+set answer [ gets $channel ]
+puts "(client) received from server: $answer"
+
+# close channel
+close $channel
---
+++ client-server/server.tcl
@@ -0,0 +1,34 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# demonstrates client-server communication (server side)
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+proc accept {channel address port} {
+	# read data from channel
+	set messageRead [ gets $channel ]
+
+	# output received data
+	puts "(server) received data: $messageRead"
+
+	# prepare answer
+	set answer "Hello, client!"
+
+	# send answer
+	puts $channel $answer
+
+	# close channel
+	close $channel
+}
+
+# define port
+set port 34567
+
+# initiate communication channel via socket
+socket -server accept $port
+
+vwait forever
