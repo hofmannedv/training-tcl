ea4fb3ca0053d4c63e3fc59d4b6bc99a036af1aa
Frank Hofmann <frank.hofmann@efho.de>
2015-06-04 23:05:00 +0200

    use case: phone book, initial commit

---
+++ use-cases/phone-book/pb-server.tcl
@@ -0,0 +1,11 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# phone book server application
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+
