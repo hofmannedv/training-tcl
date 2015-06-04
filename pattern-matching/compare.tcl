2c2ec001ad07921e408bf5e251a864da73079826
Frank Hofmann <frank.hofmann@efho.de>
2015-06-04 22:01:32 +0200

    pattern matching: compare function

---
+++ pattern-matching/compare.tcl
@@ -0,0 +1,24 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# comparing strings using string compare
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define string
+set place2 "Berlin"
+set place "Berlin-Wannsee"
+
+# define default value
+set result 42
+
+set result [ string compare $place $place2 ]
+
+if {! $result} {
+	puts "comparison result: equal"
+} else {
+	puts "comparison result: not equal"
+}
