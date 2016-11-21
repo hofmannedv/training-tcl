e70f0518e4c0ee250fc8fc607a0cb62b15181ce8
Frank Hofmann <frank.hofmann@efho.de>
2016-11-21 15:26:08 +0100

    operators: decimal to binary

---
+++ variables/binary.tcl
@@ -0,0 +1,34 @@
+#! /usr/bin/wish
+
+# -----------------------------------------------------------
+# output as binary value
+#o
+# (C) 2016 Frank Hofmann, Berlin, Germany
+# Thanks to Klaus Dierke, Eichwalde, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+proc dec2bin { decimalvalue } {
+
+	set binarystring ""
+
+	for { set i 0 } { $i < 8 } { incr i } {
+		set wert [ expr $decimalvalue & 1 ]
+		set binarystring "${wert}${binarystring}"
+		set decimalvalue [ expr $decimalvalue >> 1 ]
+	}
+
+	return $binarystring
+}
+
+# define variables (hex aa)
+set register 170
+set binarystring ""
+
+set originalvalue $register
+
+set binarystring [ dec2bin $register ]
+
+puts "Binary value of $originalvalue is $binarystring"
+
